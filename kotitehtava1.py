0f8155e56a2315b1345cc7a245cf9ea2b4114af1
Ari Kylaniemi <akylaniemi@gmail.com>
2020-10-26 15:26:12 +0200

    auts

--- kotitehtava1.py
+++ kotitehtava1.py
@@ -5,8 +5,9 @@ from math import sqrt
 
 
 print (' ')
-print ('Tehtava 1 - hypotenuusan ja/tai kateetin laskenta')
-print ('------------- + kolmion pinta-ala ---------------')
+print ('      Tehtava 1 - hypotenuusan ja/tai kateetin laskenta')
+print ('--------------------------- + kolmion pinta-ala ---------------')
+print ('--- (Pituuksia antaessasi kayta pistetta desimaalipilkkuna) ---')
 print (' ')
 
 #Kysytään käyttäjän valitsema toiminne
@@ -27,8 +28,8 @@ print (' ')
 #Käsitellään käyttäjän valinta virhekäsittelyineen:
 #Hypotenuusa ja pinta-ala
 if valinta == 1 or valinta == 3:
-    kateetti1 = int(input('Anna 1. sivu           :  '))
-    kateetti2 = int(input('Anna 2. sivu           :  '))
+    kateetti1 = float(input('Anna 1. sivu           :  '))
+    kateetti2 = float(input('Anna 2. sivu           :  '))
     hypotenuusa = sqrt(kateetti1**2 + kateetti2**2)
     print ('Hypotenuusan pituus on : ', hypotenuusa)
     #Lasketaan käyttäjän halutessa myös pinta-ala:
@@ -38,13 +39,13 @@ if valinta == 1 or valinta == 3:
 
 #Kateetti ja pinta-ala
 elif valinta == 2 or valinta == 4:
-    hypotenuusa = int(input('Anna Hypotenuusa          :  '))
-    kateetti1   = int(input('Anna sivun pituus         :  '))
+    hypotenuusa = float(input('Anna Hypotenuusa          :  '))
+    kateetti1   = float(input('Anna sivun pituus         :  '))
     kateetti2 = sqrt(hypotenuusa**2 - kateetti1**2)
     print ('Puuttuvan sivun pituus on : ', kateetti2)
     #Lasketaan käyttäjän halutessa myös pinta-ala hieman suoremmin:
     if valinta == 4:
-        print ('Pinta-ala on              : ', (kateetti1 * kateetti2) / 2) 
+        print ('Pinta-ala on              : ', (kateetti1*kateetti2)/2)
 
 #Virhekäsittely, poistutaan phjelmasta.
 else:
