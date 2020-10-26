70637b7863d8c9877f340f70143f817b59c472d5
Ari Kylaniemi <akylaniemi@gmail.com>
2020-10-26 15:40:41 +0200

    tulosten pyöristykset ja hypo/kat kokovertailu

--- kotitehtava1.py
+++ kotitehtava1.py
@@ -30,23 +30,34 @@ print (' ')
 if valinta == 1 or valinta == 3:
     kateetti1 = float(input('Anna 1. sivu           :  '))
     kateetti2 = float(input('Anna 2. sivu           :  '))
-    hypotenuusa = sqrt(kateetti1**2 + kateetti2**2)
-    print ('Hypotenuusan pituus on : ', hypotenuusa)
+    hypotenuusa  = sqrt(kateetti1**2 + kateetti2**2)
+    hypotenuusa2 = round(hypotenuusa, 2)
+    print ('Hypotenuusan pituus on : ', hypotenuusa2)
     #Lasketaan käyttäjän halutessa myös pinta-ala:
     if valinta == 3:
-        pinta = (kateetti1 * kateetti2) / 2
-        print ('Pinta-ala on           : ', pinta)
+        pinta  = (kateetti1 * kateetti2) / 2
+        pinta2 = round(pinta, 2)
+        print ('Pinta-ala on           : ', pinta2)
 
 #Kateetti ja pinta-ala
 elif valinta == 2 or valinta == 4:
     hypotenuusa = float(input('Anna Hypotenuusa          :  '))
     kateetti1   = float(input('Anna sivun pituus         :  '))
-    kateetti2 = sqrt(hypotenuusa**2 - kateetti1**2)
-    print ('Puuttuvan sivun pituus on : ', kateetti2)
-    #Lasketaan käyttäjän halutessa myös pinta-ala hieman suoremmin:
-    if valinta == 4:
-        print ('Pinta-ala on              : ', (kateetti1*kateetti2)/2)
+    if hypotenuusa > kateetti1:
+        kateetti2  = sqrt(hypotenuusa**2 - kateetti1**2)
+        kateetti22 = round(kateetti2, 2)
+        print ('Puuttuvan sivun pituus on : ', kateetti22)
+        
+        #Lasketaan käyttäjän halutessa myös pinta-ala
+        if valinta == 4:
+            pinta  = (kateetti1 * kateetti2) / 2
+            pinta2 = round(pinta, 2)
+            print ('Pinta-ala on              : ', pinta2)
 
+    else:
+        print ('Hypotenuusan olisi hyva olla pidempi kuin kateetti...')
+    
+        
 #Virhekäsittely, poistutaan phjelmasta.
 else:
     print (' ')
